cd78a48024a33f0af1713224ad2d95338a457ef6
Bui Kha Minh <buikhaminh2003@gmail.com>
2024-09-11 22:14:12 +0700

    Add Api Users

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -2,10 +2,26 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/checkstatus', (req, res, next) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'api ok'
-    })
-})
+  res.status(200).json({
+    status: 'success',
+    message: 'api ok'
+  });
+});
 
+router.get('/api/v1/users', (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Get all users',
+    metadata: [
+      {
+        name: 'John Doe',
+        email: 'join@gmai.com'
+      },
+      {
+        name: 'Jane Doe',
+        email: 'jane@gmail.com'
+      }
+    ]
+  });
+});
 module.exports = router;
