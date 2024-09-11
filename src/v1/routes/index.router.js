cad28bebf83b23d8adc84b5221cd447d16af65fd
Bui Kha Minh <buikhaminh2003@gmail.com>
2024-09-11 22:50:25 +0700

    Add api products

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -24,4 +24,38 @@ router.get('/api/v1/users', (req, res, next) => {
     ]
   });
 });
+
+router.get('/api/v1/users', (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Get all users',
+    metadata: [
+      {
+        name: 'John Doe',
+        email: 'join@gmai.com'
+      },
+      {
+        name: 'Jane Doe',
+        email: 'jane@gmail.com'
+      }
+    ]
+  });
+});
+
+router.get('/api/v1/products', (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Get all products',
+    metadata: [
+      {
+        name: 'Product 1',
+        price: 1000
+      },
+      {
+        name: 'Product 2',
+        price: 2000
+      }
+    ]
+  });
+});
 module.exports = router;
